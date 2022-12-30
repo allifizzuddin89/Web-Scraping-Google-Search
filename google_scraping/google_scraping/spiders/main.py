92d8ff15bb62af497a4457ef4b9e68a0b6db1c99
allifizzuddin89 <allifizzuddin@gmail.com>
2022-12-30 12:15:52 +0800

    Data list loop test #1

--- google_scraping/google_scraping/spiders/main.py
+++ google_scraping/google_scraping/spiders/main.py
@@ -2,6 +2,7 @@ import scrapy
 import logging
 from scrapy.utils.log import configure_logging
 import pandas as pd
+from scrapy.shell import inspect_response
 
 class MainSpider(scrapy.Spider):
     name = 'main'
@@ -9,14 +10,13 @@ class MainSpider(scrapy.Spider):
         "authority": "www.google.com",
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "accept-language": "en-US,en;q=0.9,ms;q=0.8,id;q=0.7",
-        "cache-control": "no-cache",
+        "cache-control": "max-age=0",
         "dnt": "1",
-        "pragma": "no-cache",
-        "sec-ch-ua": "\"Microsoft Edge\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"",
+        "sec-ch-ua": "\"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"108\", \"Microsoft Edge\";v=\"108\"",
         "sec-ch-ua-arch": "\"x86\"",
         "sec-ch-ua-bitness": "\"64\"",
-        "sec-ch-ua-full-version": "\"107.0.1418.62\"",
-        "sec-ch-ua-full-version-list": "\"Microsoft Edge\";v=\"107.0.1418.62\", \"Chromium\";v=\"107.0.5304.150\", \"Not=A?Brand\";v=\"24.0.0.0\"",
+        "sec-ch-ua-full-version": "\"108.0.1462.54\"",
+        "sec-ch-ua-full-version-list": "\"Not?A_Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"108.0.5359.125\", \"Microsoft Edge\";v=\"108.0.1462.54\"",
         "sec-ch-ua-mobile": "?0",
         "sec-ch-ua-model": "\"\"",
         "sec-ch-ua-platform": "\"Linux\"",
@@ -27,27 +27,29 @@ class MainSpider(scrapy.Spider):
         "sec-fetch-site": "same-origin",
         "sec-fetch-user": "?1",
         "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62"
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54"
     }
 
+
     cookies = {
-        "OTZ": "6758128_24_24__24_",
-        "SID": "RAjSALvA_Uqn4wkMXl_bNHlgrQTU1UHV4vMns_lWPLUSpLCq2BrzSDz3mUuVOeSl2vUBlg.",
-        "__Secure-1PSID": "RAjSALvA_Uqn4wkMXl_bNHlgrQTU1UHV4vMns_lWPLUSpLCqrp4fL9ds9pJaerSEkwo37A.",
-        "__Secure-3PSID": "RAjSALvA_Uqn4wkMXl_bNHlgrQTU1UHV4vMns_lWPLUSpLCq6ygAkaSyot5KnW0udFQKbg.",
-        "HSID": "AS9A4hDQZcERNbhPL",
-        "SSID": "A2i-BN379ZZlFz6tH",
-        "APISID": "NEtuYds3pSZAptTX/AFvTajtoSAd_0BH_X",
-        "SAPISID": "vNbHiIOSWrjRAayB/A1Mh9ubacBMQzpnE-",
-        "__Secure-1PAPISID": "vNbHiIOSWrjRAayB/A1Mh9ubacBMQzpnE-",
-        "__Secure-3PAPISID": "vNbHiIOSWrjRAayB/A1Mh9ubacBMQzpnE-",
-        "AEC": "AakniGMvjJqNfMo8zAvN_PjbZk0LVhMUM5rKKrd-xO3IPrLZ59DvhJihVA",
-        "NID": "511=g4WqXbWPbwgEk83BEhP_XZJQAedrglsk8IrcGxsuf0S1pziWHVcoCcLpXN5AzGSZUVUyjc8kMSF4u887QXuSyd0BWSh6INTVRFXJ5S_or-VpWh5a-MarBZ8wtGmN6Z657ie8SMmbSP1nt2AuQhXtyiAlFdjrnQdqB5g7zHcBIMfgDcin4CcFrD1EvXUM61MQkz15fT1XH9caxPbIeFCdwW-PWJtPQFW4yoQEZhJt-M045snEmkMHMPNW3CIwPKonopntpV7IwVbukFxOS9s9tXegcgBtXzCpwyL1TWEWTmd8lUWp0wK4j98UFWaVRZU7IpgVAn68YEOY93rKT0cFh_SMIXPXnPzIEmcLt98cpIlQ3S3mHOrzCxxbQmeCaZsIXsJ2ZRe3W-L-xdv9uCdPBTBZKqe_f8lD-4wZ0D-SB_RicJyLEu2m078ZvZg",
-        "1P_JAR": "2022-12-01-08",
-        "DV": "Aym1vW5bUUFYACm1QWzNqJ24pJ_MTBih720zclIcMQAAAMBLUjyomhJKOgAAAOBRX6N8aZALLQAAAKJBqsVeeJWmEQAAAA",
-        "SIDCC": "AIKkIs2owS-_afL-jdOo3XIdMZ--dT-HVyWqTww4pgGWH18-9I8JocGyAI58YYOk8Ad1c5JhmA",
-        "__Secure-1PSIDCC": "AIKkIs1PjJbz54JPdB9hDx4_baLJ2VIULltMQKLXKg6qmY5wJa4_5qpBdyKzFBJ_2CdRx5hPSw",
-        "__Secure-3PSIDCC": "AIKkIs2bjsGaatUNFUnqZmjghSKNKa-XgiOoN78cI132u0ZGcYDNOvQi-s5RlL7sj3FEqMKA8g"
+        "OTZ": "6832252_24_24__24_",
+        "AEC": "AakniGNR38W5GS2VIc05EmBUGtXVm5BMh2Sxjd_sdxjBvv9pYDK_ZG6FRA",
+        "1P_JAR": "2022-12-30-02",
+        "SID": "SQjSAFSeR3m3sRvgZf329pgx1XVt9urKFakvoIJDE2fsyPS_bNKt7-lfZYBSOGd94PhN7Q.",
+        "__Secure-1PSID": "SQjSAFSeR3m3sRvgZf329pgx1XVt9urKFakvoIJDE2fsyPS_veqiiUIXALEzGQui6X1W8w.",
+        "__Secure-3PSID": "SQjSAFSeR3m3sRvgZf329pgx1XVt9urKFakvoIJDE2fsyPS_OcGJNKj5KSZ1blqa4PZQLw.",
+        "HSID": "Anj06PpVz82PfZuPa",
+        "SSID": "Aw-AwyKkCqINq2Bjw",
+        "APISID": "zFK_bzM0aYWXsks3/Aep-ycl9fTywmW-Q4",
+        "SAPISID": "JNg7BJpEmmN1pxYi/Ar0X1QTYpgIf8vLiL",
+        "__Secure-1PAPISID": "JNg7BJpEmmN1pxYi/Ar0X1QTYpgIf8vLiL",
+        "__Secure-3PAPISID": "JNg7BJpEmmN1pxYi/Ar0X1QTYpgIf8vLiL",
+        "NID": "511=OBirSfPwvj-8lEiiQ5ORjBAM1MW_lip8E8F6BQpdFrbbcrakoq_bGK98LjLE9fVj2ev8NDkvVcg1ujTnpxy5jmXoLQYINZu2Hmb4R2xuCWOhLlP9dp6Vc3LHuZIJs7wY0CLVNvsqUmDfNg7tgp7q6_T3bfNX3cox5ZVUpxZ75_vsWHgwUKnGKylloHDMm1dqZ_sncj22qCW1K_kZSFh16ovF9KAk-55ATlsoWbcO1u_L5APogHf24XBEH1ENHurq-HGgL365Ja8vnGo_N84vMA4J9yABSLTZpmYarO1dSi9hLlgyNH2j9RR6XfMM-Q3ShoKIqYQ",
+        "DV": "Aym1vW5bUUFYIBqkWuyFV2m6_mQPVhgvSfGgakooqQAAAEDoe9uMnBRHXAAAAOBRX6N8aZALYQAAAJBSG8TWjNqJGAAAAA",
+        "UULE": "a+cm9sZTogMQpwcm9kdWNlcjogMTIKdGltZXN0YW1wOiAxNjcyMzY5MDM2NTM5MDAwCmxhdGxuZyB7CiAgbGF0aXR1ZGVfZTc6IDI5NzUwMTIwCiAgbG9uZ2l0dWRlX2U3OiAxMDE1NzU0NzEwCn0KcmFkaXVzOiA3MTkyMApwcm92ZW5hbmNlOiA2Cg==",
+        "SIDCC": "AIKkIs1tEoZbPqFLQbSSUjRxxC2EhgqXnnw26OoX9lgUEW8p850iFSCWM_mLfwAmbutW7L1Z-LM",
+        "__Secure-1PSIDCC": "AIKkIs3XxBCDMBEF3YW6Z-eENFoxcsZqBMjXrwAGWMN5RJumV5y9HM82rdlnjXya7CnmjO2ddA",
+        "__Secure-3PSIDCC": "AIKkIs2O_nDwhb8sVcxqCKhR_mXD0GFOQWtPwas1XwXvcEA-jpJIR62HmdBusYXKwSxzt2Zm-vQ"
     }
 
     configure_logging(
@@ -75,16 +77,17 @@ class MainSpider(scrapy.Spider):
         # print('\n')
         # print(response.text)
         # print('\n')
-        # inspect_response(response, self)
-        item1 = response.css('div.MjjYud')
-        item2 = response.css('div.hlcw0c')
-        item3 = response.css('div.ULSxyf')
-        items = []
-        while items in item1 or items in item2 or items in item3:
-            item = {
-                'Title' : response.css('div.LC20lb.MBeuO.DKV0Md::text').get(),
-                'Link' : response.css('div.div.yuRUbf>a::attr(href)').get(),
-                'Description' : response.css('div.VwiC3b>span::text').get(),
-                'Tags' : response.css('div.HiHjCd>a[href]::text').getall(),
-            }
-            yield item
+        inspect_response(response, self)
+        # item1 = response.css('div.MjjYud')
+        # item2 = response.css('div.hlcw0c')
+        # item3 = response.css('div.ULSxyf')
+        # items = []
+        # while items in item1 or items in item2 or items in item3:
+        #     item = {
+        #         'Title' : items.css('div.LC20lb.MBeuO.DKV0Md::text').get(),
+        #         'Link' : items.css('div.div.yuRUbf>a::attr(href)').get(),
+        #         'Description' : items.css('div.VwiC3b>span::text').get(),
+        #         'Tags' : items.css('div.HiHjCd>a[href]::text').getall(),
+        #     }
+        #     yield item
+
